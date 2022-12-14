c699b790202820371cb137c318c2711df519b4f9
Artem <artchzhen@gmail.com>
2022-12-14 19:05:09 +0500

    init devolopment

---
+++ Ministone/Ministone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ministone
+//
+//  Created by Артем Чжен on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
